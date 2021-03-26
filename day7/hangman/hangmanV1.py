d2a520a16ce7b979c28ce012e254d1b37a5de744
js-rushing <unoriginal9000@yahoo.com>
2021-03-26 15:12:04 -0500

    Through Day 7

---
+++ day7/hangman/hangmanV1.py
@@ -0,0 +1,67 @@
+import random
+from hangman_art import hangmanArt
+from hangman_art import gallowsArr
+from hangman_words import wordList
+
+# Gameplay Variables
+gameOver = False
+wrong = 0
+
+# Set game word
+word = random.choice(wordList).upper()
+wordArr = [ch for ch in word]
+
+# Make array and corresponding string for guess tracking and comparison
+guessingArr = []
+for n in wordArr:
+    guessingArr.append("_")
+
+guessingStr = ""
+for n in word:
+    guessingStr += "_"
+
+alreadyGuessedArr = []
+
+
+# Function to display strings with a space between characters
+def printWithSpaces(theString):
+    count = 0
+    formattedStr = ""
+    for n in theString:
+        formattedStr += theString[count]
+        formattedStr += " "
+        count += 1
+    print(formattedStr)
+
+
+# Begin Game
+print(hangmanArt)
+
+while (not gameOver and (guessingStr != word)):
+    print(gallowsArr[wrong])
+    printWithSpaces(guessingStr)
+    guess = input("Guess a letter: ").upper()
+    while (guess in alreadyGuessedArr):
+        print(f"You've already guessed {guess.upper()}")
+        guess = input("Guess a different letter: ").upper()
+    alreadyGuessedArr.append(guess)
+
+    if (guess in wordArr):
+        step = -1
+        for n in wordArr:
+            step += 1
+            if wordArr[step] == guess:
+                guessingArr[step] = guess
+        guessingStr = ''.join(guessingArr)
+    else:
+        wrong += 1
+        if wrong == len(gallowsArr)-1:
+            print(gallowsArr[wrong])
+            print("Game Over.")
+            print("You lose.")
+            print(f"The word is {word}")
+            gameOver = True
+
+if (guessingStr == word):
+    print(word)
+    print("You win!")
---
+++ day7/hangman/hangman_art.py
@@ -0,0 +1,83 @@
+hangmanArt = ('''
+ _
+| |
+| |__   __ _ _ __   __ _ _ __ ___   _ _ _ __
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \\
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |
+                   |___/
+''')
+
+gallowsArtNew = ('''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+==========
+''')
+
+gallowsArtHead = ('''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+==========
+''')
+
+gallowsArtBody = ('''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+==========
+''')
+
+gallowsArtLeftArm = ('''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+==========
+''')
+
+gallowsArtRightArm = ('''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+==========
+''')
+
+gallowsArtLeftLeg = ('''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+==========
+''')
+
+gallowsArtRightLeg = ('''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+==========
+''')
+
+gallowsArr = [gallowsArtNew, gallowsArtHead, gallowsArtBody,
+              gallowsArtLeftArm, gallowsArtRightArm, gallowsArtLeftLeg, gallowsArtRightLeg]
