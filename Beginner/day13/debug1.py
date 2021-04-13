7eda435c5592ba815c0d9c96ac5bfba3ac4e27a3
js-rushing <unoriginal9000@yahoo.com>
2021-04-13 10:22:38 -0500

    Through Day 13

---
+++ Beginner/day13/debug1.py
@@ -0,0 +1,95 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# In order for 20 to be included, range must be 1-21
+# from random import randint
+
+
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You got it")
+
+
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+# List indeces range from 0-5
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+# People born in 1994 were excluded
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#     print("You are a millenial.")
+# elif year >= 1994:
+#     print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# input must be converted to integer
+# print statement should print an f-string
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# When getting the word_per_page input '==' used rather than '='
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+
+# There was an indentation error
+# 'b_list.append...' was outside the for loop
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
